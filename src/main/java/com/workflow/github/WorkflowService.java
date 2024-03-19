71e15ad7ef9f5abac5d0c380361cb4965db14993
cesar <cesaruni84@gmail.com>
2024-03-18 23:18:37 -0500

    first commit

--- src/main/java/com/workflow/github/WorkflowService.java
+++ src/main/java/com/workflow/github/WorkflowService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class WorkflowService {
     public String message() {
-        return "Hello";
+        return "Hello v2";
     }
 }
